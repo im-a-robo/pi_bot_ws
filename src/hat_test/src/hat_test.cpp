490dd8ac7f17efdcc7268341cb9b051a0b11de58
im-a-robo <nmdas05x@gmail.com>
2025-07-23 12:36:10 -0400

    updated hat test to test all dc features

--- src/hat_test/src/hat_test.cpp
+++ src/hat_test/src/hat_test.cpp
@@ -20,16 +20,24 @@ class HatTestNode : public rclcpp::Node {
 
    private:
     void timer_callback() {
-        motor_pwr = (motor_pwr + 1) % 50;
+        invert = motor_pwr % 101 == 0 ? invert *= -1 : invert;
+        motor_pwr = ((motor_pwr + 1) % 101) * invert;
         RCLCPP_INFO(this->get_logger(), "Running hat test node, setting motor power to %d",
                     motor_pwr);
 
         auto motor1 = hat_.get_motor(1);
+        auto motor2 = hat_.get_motor(2);
+        auto motor3 = hat_.get_motor(3);
+        auto motor4 = hat_.get_motor(4);
         motor1->set_power(motor_pwr);
+        motor2->set_power(motor_pwr);
+        motor3->set_power(motor_pwr);
+        motor4->set_power(motor_pwr);
     }
 
     rclcpp::TimerBase::SharedPtr timer_;
     int motor_pwr = 0;
+    int invert = 1;
     MotorHat hat_;
 };
 
